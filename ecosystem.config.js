6dc38ee2b4cbdba3f2429d245ed7ab76162343ef
John Heeter <john@Boid.com>
2019-09-02 13:07:46 -0500

    reduced cron frequency

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,19 +4,19 @@ module.exports = {
       name: 'cron1',
       script: 'cron/runCronGroup.js',
       args: '1',
-      restart_delay: 600000,
+      restart_delay: 1200000,
     },
     {
       name: 'getBoincWork',
       script: 'cron/runSingleCron.js',
       args: 'getBoincWork',
-      restart_delay: 120000,
+      restart_delay: 300000,
     },
     {
       name: 'getRvnWork',
       script: 'cron/runSingleCron.js',
       args: 'getRvnWork',
-      restart_delay:300000,
+      restart_delay:600000,
     }
   ]
 }
