a5e68c90b5c84160b31e4ce47f3a35160f806259
john-boid <john@boid.com>
2019-10-01 01:58:44 -0500

    add cleanup cron

---
+++ cron/jobs/dbCleanup.js
@@ -0,0 +1,25 @@
+const db = require('../../db.js')
+const logger = require('logging').default('dbCleanup')
+const ms = require('human-interval')
+
+
+async function init(){
+  var lookback = require('../../.env').cleanupLookback
+  if (!lookback) lookback = "1 week"
+  const sinceTime = new Date(Date.now() - ms(lookback)).toISOString()
+  logger.info('Deleting older than:',sinceTime)
+  let results = {
+    sinceTime,
+    deletedShareDatas: (await db.gql(`mutation{deleteManyShareDatas(where:{time_lt:"${sinceTime}"}){count}}`)).count,
+    deletedWorkUnits: (await db.gql(`mutation{deleteManyWorkUnits(where:{validatedAt_lt:"${sinceTime}"}){count}}`)).count,
+    deletedPowerRatings: (await db.gql(`mutation{deleteManyDevicePowerRatings(where:{createdAt_lt:"${sinceTime}"}){count}}`)).count
+  }
+  return {results}
+}
+
+
+
+
+module.exports = init
+if (process.argv[2] === 'dev') init().catch(logger.error)
+
