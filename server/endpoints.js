36dbc9daf178502014869547648c1d11b66340ce
Boid <john@boid.com>
2020-04-26 21:52:15 -0500

    Update endpoints.js

--- server/endpoints.js
+++ server/endpoints.js
@@ -1,16 +1,12 @@
 module.exports = [
 'https://eos.greymass.com',
-'https://api.eosnewyork.io',
-'https://eos.greymass.com:443',
 'https://api.eosn.io',
 'https://api.main.alohaeos.com:443',
 'https://node1.eosphere.io',
 'https://node2.eosphere.io',
-'https://publicapi-mainnet.eosauthority.com',
 'https://mainnet.meet.one',
 'https://api.eoslaomao.com',
 'https://api.sheos.org',
 'https://mainnet.eosamsterdam.net',
-'https://eos.eoscafeblock.com',
 'https://eos.api.boid.com'
 ]
