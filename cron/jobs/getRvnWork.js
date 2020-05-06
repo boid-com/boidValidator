eb393025d683e6c99f80f82745fa9030ff3f2b19
John Heeter <john@boid.com>
2020-05-06 18:48:24 -0500

    rvnGetWork talks to nanopool, still needs tweaking

--- cron/jobs/getRvnWork.js
+++ cron/jobs/getRvnWork.js
@@ -4,11 +4,12 @@ const env = require('../../.env.json')
 const sleep = ms => new Promise(res => setTimeout(res, ms))
 const logger = require('logging').default('getRvnWork')
 const ms = require('human-interval')
+const rvnAddress = 'RFwjWWCLbKQQjTtAjwZeRb2HzqT1Jkpfff'
 
 ax.defaults.timeout = 20000
 // ax.defaults.headers.common['auth'] = ''
 ax.defaults.headers.common['Content-Type'] = 'application/json'
-ax.defaults.baseURL = 'https://boid.x16rv2.jamiec.org/api'
+ax.defaults.baseURL = 'https://api.nanopool.org/v1/rvn'
 
 logger.info(ax.defaults.baseURL)
 var hash = require('node-object-hash')({ sort: false, coerce: false }).hash
@@ -16,10 +17,10 @@ var workers
 
 async function createShareData (share, deviceId) {
   var valid
-  if (share.valid = 1) valid = true
-  else valid = false
-  const time = new Date(share.time * 1000)
-  const shareHash = hash(share, { alg: 'rsa-sha1' })
+  // if (share.valid = 1) valid = true
+  // else valid = false
+  const time = new Date(share.date * 1000)
+  const shareHash = hash(JSON.stringify(share), { alg: 'rsa-sha1' })
   const result = db.gql(`
     mutation($time:DateTime!){
       upsertShareData(
@@ -27,11 +28,11 @@ async function createShareData (share, deviceId) {
         update:{}
         create:{
           shareHash: "${shareHash}"
-          shareId: ${share.id}
-          valid: ${valid}
+          shareId: ${parseInt(shareHash)}
+          valid: true
           time:$time
-          difficulty: ${share.difficulty}
-          shareDifficulty: ${share.share_diff}
+          difficulty: ${share.shares * 1000}
+          shareDifficulty: ${share.shares * 1000}
           deviceId: "${deviceId}"
         }
       ){time}
@@ -45,7 +46,12 @@ async function doQuery (worker, ModTime) {
     try {
       try {
         logger.info('Getting RvnShares...')
-        var shares = (await ax.get(`/getWorkerShares/${worker.id}?since=${ModTime}`)).data
+        var shares = (await ax.get(`/hashratechart/${rvnAddress}/${worker.id}`)).data.data
+        .map(el => { 
+          el.date = parseInt(el.data)*1000
+          return el
+        }).filter(el => parseInt(el.date)*1000 > ModTime)
+        console.log(shares)
         logger.info('Downloaded RvnShares:', shares.length)
       } catch (error) {
         console.log(error)
@@ -54,7 +60,7 @@ async function doQuery (worker, ModTime) {
       if (shares.length === 0) return res()
       logger.info('Writing RvnShares to DB...')
       for (var share of shares) {
-        await createShareData(share, worker.worker)
+        await createShareData(share, worker.id)
       }
       logger.info('Finished writing RvnShares to DB')
       res()
@@ -68,20 +74,20 @@ async function doQuery (worker, ModTime) {
 
 async function init () {
   try {
-    workers = (await ax.get('/getWorkers')).data.filter(el => el.worker != '')
+    workers = (await ax.get('/user/'+rvnAddress)).data.data.workers.filter(el => parseFloat(el.h24) >= 0.1)
     logger.info('')
     logger.info('Found', workers.length, 'rvnWorkers')
 
     let ModTime
-    const lastRun = (await db.gql(`{ cronRuns( last:1 where: {runtime_not:null job: { name: "getRvnWork" } }) {
-      errors runtime createdAt } }`))[0]
-    if (!lastRun || lastRun.errors.length > 0) ModTime = parseInt(Date.now() - ms('two hours'))
-    else ModTime = parseInt(Date.parse(lastRun.createdAt) - ms('one minute'))
-    logger.info('Getting RvnShares since:', new Date(ModTime).toLocaleString())
-
+    // const lastRun = (await db.gql(`{ cronRuns( last:1 where: {runtime_not:null job: { name: "getRvnWork" } }) {
+    //   errors runtime createdAt } }`))[0]
+    // if (!lastRun || lastRun.errors.length > 0) ModTime = parseInt(Date.now() - ms('two hours'))
+    // // else ModTime = parseInt(Date.parse(lastRun.createdAt) - ms('one minute'))
+    // logger.info('Getting RvnShares since:', new Date(ModTime).toLocaleString())
+    ModTime = parseInt(Date.now() - ms('two hours'))
     for (worker of workers) {
       logger.info('')
-      logger.info('RvnDevice:', worker.worker)
+      logger.info('RvnDevice:', worker.id)
       await doQuery(worker, ModTime)
     }
     logger.info('getRvnWork has finished!')
