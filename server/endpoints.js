ef82a555a94e95c300bcffb70314b078250a4a93
John Heeter <john@boid.com>
2020-07-16 01:53:38 -0500

    endpoints

--- server/endpoints.js
+++ server/endpoints.js
@@ -1,12 +1,10 @@
 module.exports = [
 'https://eos.greymass.com',
 'https://api.eosn.io',
-'https://api.main.alohaeos.com:443',
-'https://node1.eosphere.io',
-'https://node2.eosphere.io',
-'https://mainnet.meet.one',
-'https://api.eoslaomao.com',
-'https://api.sheos.org',
-'https://mainnet.eosamsterdam.net',
-'https://eos.api.boid.com'
+'https://api.main.alohaeos.com',
+'https://api.eosargentina.io',
+'https://mainnet.genereos.io',
+'https://api.eos.cryptolions.io',
+'https://api.eosarabia.net',
+'https://api.eoseoul.io',
 ]
